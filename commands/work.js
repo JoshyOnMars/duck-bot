15c41b9b80f223a93eff2ffca265a9d79354d10b
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-06 06:14:36 +0100

    Update work.js

--- commands/work.js
+++ commands/work.js
@@ -1,8 +1,9 @@
 const { MessageEmbed } = require("discord.js")
+const { pagination } = require("reconlx")
 
 module.exports = {
   name: "work",
-  cooldown: 3600,
+  cooldown: 0,
   async execute(message, args, client) {
     const jobs = [
       {job: "YouTuber", salary: 10000},
@@ -12,6 +13,40 @@ module.exports = {
     console.log(`${i.job}: ${i.salary} coins per hour`)
     }
     
+    const chunk = (array, chunkSize = 15) => {
+            const chunked = [];
+            for(let i = 0; i < array.length; i += chunkSize) {
+                chunked.push(array.slice(i, i + chunkSize));
+            }
+
+            return chunked;
+        }
+
+        // This is used by the library
+        const embedPages = [];
+
+        // Store all client commands in this array.
+        const clientCommands = [];
+        for(const [job, {salary}] of jobs) {
+            jobList.push({ name: job, value: `<:reply:881608199143325767>Salary: ${salary || "none"}` });
+        }
+        // Chunking the commands in smaller arrays. Leave second parameter blank to use default of 15, or choose your own size.
+        // Note that Discord doesn't allow any embed to go over 25 fields, 2000 characters.
+        chunk(jobList, 6).forEach((chunks) => {
+            const embed = new MessageEmbed().setColor("YELLOW").setAuthor(`Job List`, client.user.displayAvatarURL(), 'https://discord-power-bot.glitch.me/commands.html').setFooter(`${client.prefix}`);
+            embed.addFields(chunks);
+            embedPages.push(embed);
+        });
+        
+        pagination({
+            embeds: embedPages,
+            channel: message.channel,
+            author: message.author,
+            time: 15000,
+        });
+
+        message.channel.send({ embeds: [embed] })
+
     //const response = await profileModel.findOneAndUpdate(
     //  {
     //    userID: message.author.id,
