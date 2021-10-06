c9dfa12c5d550a9863095fc800b8ade2cb01d096
JoshyOnMars <89095695+JoshyOnMars@users.noreply.github.com>
2021-10-06 06:07:21 +0100

    Create work.js

---
+++ commands/work.js
@@ -0,0 +1,27 @@
+const { MessageEmbed } = require("discord.js")
+
+module.exports = {
+  name: "work",
+  cooldown: 3600,
+  async execute(message, args, client) {
+    const jobs = [
+      {job: "YouTuber", salary: 10000},
+      {job: "Streamer", salary: 15000}
+    ]
+    for (const i of jobs) {
+    console.log(`${i.job}: ${i.salary} coins per hour`)
+    }
+    
+    //const response = await profileModel.findOneAndUpdate(
+    //  {
+    //    userID: message.author.id,
+    //  },
+    //  {
+    //    $inc: {
+    //      coins: randomNumber,
+    //    },
+    //  }
+    //);
+    //return message.channel.send(`${message.author}, You worked and you earnt ${randomNumber} coins!`);
+  },
+};
